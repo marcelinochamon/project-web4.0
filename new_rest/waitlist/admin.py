aad72aa08593754b6793a97697035a33a6f28d9b
Jeffrey-Leu <98000977+Jeffrey-Leu@users.noreply.github.com>
2022-04-28 17:22:54 -0500

    Update

--- new_rest/waitlist/admin.py
+++ new_rest/waitlist/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-# from .models import Friend
+from .models import Wait
+
+class WaitAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'party_size', 'dining_time', 'seated', 'left')
 
 # Register your models here.
-# admin.site.register(Friend)
+admin.site.register(Wait)
