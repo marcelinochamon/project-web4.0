54a2cb4123de5646859ab8c39cc87a8f6e739f3c
hh15248 <heather.hannan.524@gmail.com>
2022-04-29 16:50:23 -0500

    added comments

--- new_rest/waitlist/assignment.py
+++ new_rest/waitlist/assignment.py
@@ -1,6 +1,21 @@
 from datetime import datetime
 from django.utils import timezone
+
 tz = timezone.get_default_timezone()
+
+# RETURNS BEST PERSON IN LIST OF WAITING PEOPLE FOR A GIVEN TABLE
+# ATTRIBUTES TO ACCESS: table.seats, table.server, 
+# for person in waiting_people
+# person.party_size, person.wait_time, person.arrival_time
+def find_best_person(table, waiting_people):
+    """
+    Function finds the best party in list of waiting people to assign to a given empty table 
+    inputs: Table object, list of Wait objects
+    return: Wait object
+    """
+    # Get new party from top of waitlist
+    new_party = waiting_people.pop()
+    return new_party
 
 
 def assign_tables():
@@ -16,8 +31,7 @@ def assign_tables():
             print("Nobody on waitlist")
             break
         else:
-            # Get new party from top of waitlist
-            new_party = waiting_people.pop()
+            new_party = find_best_person(table, waiting_people)
             # Assign table party the new name
             table.party = new_party.name
             # Initialize seated time
