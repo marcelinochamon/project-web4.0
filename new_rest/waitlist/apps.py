f1697ba1edfec19ac66c041d623d730d4975c3f7
hh15248 <heather.hannan.524@gmail.com>
2022-04-29 12:50:32 -0500

    assinging tables!

--- new_rest/waitlist/apps.py
+++ new_rest/waitlist/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 import schedule
 import time
+from multiprocessing import Process
 from datetime import datetime
 from django.utils import timezone
 import threading
@@ -17,34 +18,38 @@ class WaitlistConfig(AppConfig):
         tz = timezone.get_default_timezone()
         from .models import Table, Wait
         def start_thread(**kwargs):
-            def scheduler():
+            def scheduler(**kwargs):
                 def assign_tables():
-                    print("I worked")
-                    empty_tables = Table.objects.filter(party = "Empty")
-                    waiting_people = Wait.objects.all().order_by("arrival_time")
-                    print(empty_tables)
-                    # for table in empty_tables:
-                    #     print(len(table))
-                    #     if len(waiting_people) == 0:
-                    #         print("I want to break")
-                    #         # break
-                    #     else:
-                    #         print("I made it here")
-                    #         # new_party = waiting_people.pop()
-                    #         # table.party = new_party.name
-                    #         # table.time_seated = datetime.now(tz)
-                    #         # table.save()
-                    #         # new_party.delete()
-    
-
-                schedule.every(2).seconds.do(assign_tables)
+                    # Get empty tables
+                    empty_tables = list(Table.objects.filter(party = "Empty"))
+                    # Get list of people on waitlist, ordered by longest to shortest wait time
+                    waiting_people = list(Wait.objects.all().order_by("arrival_time"))
+                    # Loop through empty tables
+                    for table in empty_tables:
+                        # Check if anyone is waiting on the waitlist
+                        if len(waiting_people) == 0:
+                            print("Nobody on waitlist")
+                            break
+                        else:
+                            # Get new party from top of waitlist
+                            new_party = waiting_people.pop()
+                            # Assign table party the new name
+                            table.party = new_party.name
+                            # Initialize seated time
+                            table.time_seated = datetime.now(tz)
+                            # Save new Table object
+                            table.save()
+                            # Delete off the waitlist
+                            new_party.delete()
+                            print("Assigned Party {} to table {}".format(table.party, table.number))
+                schedule.every(10).seconds.do(assign_tables)
                 while True:
                     schedule.run_pending()
                     time.sleep(2)
-            t = threading.Thread(target = scheduler, kwargs = {})
-            t.setDaemon(True)
-            t.start()
+            scheduler()
+        p = Process(target = start_thread, args = ())
+        p.start()
 
-        # connection_created.connect(start_thread)
+
 
 
