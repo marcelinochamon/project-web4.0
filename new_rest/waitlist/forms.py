940e84f7a0591af2af406d1664b6f15032c4010f
Jeffrey-Leu <98000977+Jeffrey-Leu@users.noreply.github.com>
2022-05-01 14:16:40 -0500

    Config

--- new_rest/waitlist/forms.py
+++ new_rest/waitlist/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Wait
+from .models import Wait, Config
 
 class WaitForm(forms.ModelForm):
     name = forms.CharField(max_length=20)
@@ -22,4 +22,24 @@ class AssignForm(forms.ModelForm):
 
         fields = [
             'table',
+        ]
+        
+class ConfigForm(forms.ModelForm):
+    number_of_servers = forms.IntegerField()
+    server_names = forms.CharField(max_length = 50)
+    tables_for_2 = forms.IntegerField()
+    tables_for_4 = forms.IntegerField()
+    tables_for_6 = forms.IntegerField()
+    tables_for_8 = forms.IntegerField()
+
+    class Meta:
+        model = Config
+
+        fields = [
+            'number_of_servers',
+            'server_names',
+            'tables_for_2',
+            'tables_for_4',
+            'tables_for_6',
+            'tables_for_8'
         ]
